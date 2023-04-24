91dc4197be4e046b83a8603a39ae10c8d19c9252
Donald Akite <donaldakite27@gmail.com>
2023-04-24 00:19:38 +0000

    Update dynamic content

--- index.js
+++ index.js
@@ -1,76 +1,69 @@
-const humburg = document.querySelector('#MENU');
-const popup = document.querySelector('#menupopup');
-humburg.addEventListener('click', () => {
-  popup.style.display = 'flex';
-});
-popup.addEventListener('click', () => {
-  popup.style.display = 'none';
-});
-
-const load = document.querySelector('.mobile-speakers');
-
-const speakers = [
+const featuredSpeakers = [
   {
-    image: 'img/speaker1.jpg',
-    name: 'Gary Mckinnon',
-    about: 'Administrator from Scotland.',
-    more: 'He had been in the process for more than one year.',
+    picture: 'images/assets/speaker_01.png',
+    name: 'Yochai Benkler',
+    presentation: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    description: 'Benkler studies commons-based peer productions, and published his seminal book The Wealth of Network in 2006.',
   },
   {
-    image: 'img/Falgun-Rathod.jpg',
-    name: 'Falgun-Rathod',
-    about: 'student from tokyo.',
-    more: 'He had been in the process for more than one year.',
+    picture: 'images/assets/speaker_02.png',
+    name: 'SohYeong Noh',
+    presentation: 'Director of Art Centre Nabi and a board member of CC Korea',
+    description: 'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities and the arts.',
   },
   {
-    image: 'img/speaker2.jpg',
-    name: 'Donald Mckinnon',
-    about: 'Administrator from Scotland.',
-    more: 'He had been in the process for more than one year.',
+    picture: './images/assets/speaker_03.png',
+    name: 'Lila Tretikov',
+    presentation: 'Executive Director of the Wikimedia Foundation',
+    description: 'Lila Tretikov is the Executive Director of the Wikimedia Foundation, the nonprofit organization that operates Wikipedia. Wikipedia is freely available in 290 languages and used by nearly half a billion people around the world every month.',
   },
   {
-    image: 'img/kevin_mitnick.jpg',
-    name: 'Keven Mitnick',
-    about: 'Administrator from Scotland.',
-    more: 'He had been in the process for more than one year.',
+    picture: './images/assets/speaker_04.png',
+    name: 'Kilnam Chon',
+    presentation: 'CEO of COWBERY',
+    description: "Kilnam Chon is help to bring the internet to Asia and is an outspoken advocate for the open web and digital commons. in 2012 he was inducted into the inaugural class of the Internet Society's (ISOC) Internet Hall of Fame",
   },
   {
-    image: 'img/maxresdefault.jpg',
-    name: 'Max',
-    about: 'Administrator from Scotland.',
-    more: 'He had been in the process for more than one year.',
+    picture: './images/assets/speaker_05.png',
+    name: 'Julia Leda',
+    presentation: 'President of Young Pirates of Europe',
+    description: "European integration, political democracy and participation of youth trough online as her major condern, Reda's report outlining potential changes to EU copyright law was approved by the Parliament in July.",
   },
   {
-    image: 'img/speaker3.jpeg',
-    name: 'Donald Akite',
-    about: 'Administrator from Scotland.',
-    more: 'He had been in the process for more than one year.',
+    picture: './images/assets/speaker_06.png',
+    name: 'Ryan Merkley',
+    presentation: 'CEO of Creative Commons, ex COO of the Mozilla foundation',
+    description: 'Ryan had been leading open-source projects at the Mozilla Foundation such as the open source movement.',
   },
 ];
 
-speakers.forEach((Project) => {
-  const content = `
-  <div class = "spaeaker-card">
-  <div class="speaker">
-      <div class="card-img">
-          <img src="${Project.image}" alt="">
-      </div>
-      <div class="card-detail">
-          <div class="name">
-              <h3>${Project.name}</h3>
-          </div>
-          <div class="about">
-              <p>${Project.about} </p>
-          </div>
-          <div class="divider">
-
-          </div>
-          <div class="more">
-              <p>${Project.more} </p>
-          </div>
-      </div>
-  </div>
-</div>
+// Iterate through the array and create a new div for each speaker
+featuredSpeakers.forEach((speaker) => {
+  const speakerDiv = document.createElement('div');
+  speakerDiv.innerHTML = `
+    <div class="mx-4 my-10 flex">
+    <img class="w-32 h-32" src="${speaker.picture}" alt="${speaker.name}">
+    <div class="px-4 flex flex-col justify-between">
+        <p class="font-bold text-xl">${speaker.name}</p>
+        <p class="text-xs text-[#F04A1C] py-2 md:text-sm">${speaker.presentation}</p>
+        <hr class="py-2"/>
+        <p class="text-xs md:text-sm">${speaker.description}</p>
+    </div>
+    </div>
     `;
-  load.innerHTML += content;
+  document.querySelector('.speakers').appendChild(speakerDiv);
+  // Add the speakers to the DOM
+  document.querySelector('.speakers').appendChild(speakerDiv);
+});
+
+document.querySelector('.hamburger').addEventListener('click', () => {
+  document.querySelector('.mobile-menu').classList.toggle('hidden');
+  document.querySelector('.hamburger').classList.toggle('hidden');
+  document.querySelector('.close-btn').classList.toggle('hidden');
+});
+
+document.querySelector('.close-btn').addEventListener('click', () => {
+  document.querySelector('.mobile-menu').classList.toggle('hidden');
+  document.querySelector('.hamburger').classList.toggle('hidden');
+  document.querySelector('.close-btn').classList.toggle('hidden');
 });
