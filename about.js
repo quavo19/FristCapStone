5e42721809fac537bd51935ff171e7cd0ee4aabb
Donald Akite <donaldakite27@gmail.com>
2023-04-24 00:18:29 +0000

    Fix menu button

---
+++ about.js
@@ -0,0 +1,11 @@
+document.querySelector('.hamburger').addEventListener('click', () => {
+  document.querySelector('.mobile-menu').classList.toggle('hidden');
+  document.querySelector('.hamburger').classList.toggle('hidden');
+  document.querySelector('.close-btn').classList.toggle('hidden');
+});
+
+document.querySelector('.close-btn').addEventListener('click', () => {
+  document.querySelector('.mobile-menu').classList.toggle('hidden');
+  document.querySelector('.hamburger').classList.toggle('hidden');
+  document.querySelector('.close-btn').classList.toggle('hidden');
+});
